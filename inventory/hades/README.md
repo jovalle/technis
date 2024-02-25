85de5af268fc861fd9bc55d059251027cfa0f22d
Jay Ovalle <jay.ovalle@gmail.com>
2024-02-25 09:41:03 -0500

    Add k3sup for k8s deployment

--- inventory/hades/README.md
+++ inventory/hades/README.md
@@ -1,9 +1,11 @@
-# Hades
+# Technis
 
 ## Context
 
 Comprises of baremetal and virtual machines. Home to my media/data management apps and is roughly my "production" environment.
 
-### Naming Convention
+`deploy.sh` uses `k3sup` to very easily deploy a `k3s` cluster with unprecedented idempotency. My preferred method of installing and maintaining a Kubernetes cluster.
 
-Loved the game and the universe it trapped me in. All nodes in this cluster are named after in-game characters.
+### Host Naming Convention
+
+All hosts are named after characters from `Hades the Game`. Loved the game and the universe it transported me to while playing.
--- inventory/hades/main.tf
+++ inventory/hades/main.tf
@@ -1,82 +1,75 @@
 # Set credentials for vSphere provider
-# export VSPHERE_USER="administrator@vsphere.local"
+# export VSPHERE_USER=""
 # export VSPHERE_PASSWORD=""
-# export VSPHERE_SERVER=mothership.technis.net
+# export VSPHERE_SERVER=""
 provider "vsphere" {
-    # Self-signed cert for now
-    allow_unverified_ssl = true
+  # Self-signed cert for now
+  allow_unverified_ssl = true
 }
 
 data "vsphere_datacenter" "dc" {
-    name = "technis"
+  name = "technis"
 }
 
 data "vsphere_compute_cluster" "cluster" {
-    name = "local"
-    datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "local"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_datastore" "datastore" {
-    name = "NVMe"
-    datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "NVMe"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_host" "host" {
-    name = "nexus.technis.net"
-    datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "core.technis.net"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_network" "network" {
-    name = "VM Network"
-    datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "VM Network"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 data "vsphere_virtual_machine" "template" {
-    name = "Templates/ubuntu-server-2204"
-    datacenter_id = data.vsphere_datacenter.dc.id
+  name          = "templates/debian12"
+  datacenter_id = data.vsphere_datacenter.dc.id
 }
 
 locals {
-   vms = {
-    "zagreus"  = { name = "Ubuntu Server - zagreus", mac_addr = "68:61:64:65:73:25" },
+  vms = {
+    "cerberus" = { mac_addr = "68:61:64:65:73:11" },
+    "zagreus"  = { mac_addr = "68:61:64:65:73:12" },
+    "thanatos" = { mac_addr = "68:61:64:65:73:13" },
+    "orpheus"  = { mac_addr = "68:61:64:65:73:14" },
   }
 }
 
 resource "vsphere_virtual_machine" "vm" {
-    for_each = local.vms
+  for_each = local.vms
 
-    name = each.value.name
-    resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
-    datastore_id = data.vsphere_datastore.datastore.id
+  name             = each.key
+  resource_pool_id = data.vsphere_compute_cluster.cluster.resource_pool_id
+  datastore_id     = data.vsphere_datastore.datastore.id
 
-    num_cpus = 8
-    num_cores_per_socket = 8
-    memory = 16384
-    guest_id = "ubuntu64Guest"
+  num_cpus             = 8
+  num_cores_per_socket = 8
+  memory               = 16384
+  guest_id             = "debian11_64Guest"
 
-    # GPU passthrough
-    # memory_reservation = 16384
-    # host_system_id = data.vsphere_host.host.id
-    # pci_device_id = ["0000:82:00.0"]
-    # extra_config = {
-    #     "hypervisor.cpuid.v0"         = "false"
-    #     "pciPassthru.use64bitMMIO"    = "true"
-    #     "pciPassthru.64bitMMIOSizeGB" = "32"
-    # }
+  network_interface {
+    network_id     = data.vsphere_network.network.id
+    use_static_mac = true
+    mac_address    = each.value.mac_addr
+  }
 
-    network_interface {
-        network_id = data.vsphere_network.network.id
-        use_static_mac = true
-        mac_address = each.value.mac_addr
-    }
+  disk {
+    label            = "disk0"
+    size             = 480
+    thin_provisioned = true
+  }
 
-    disk {
-        label = "disk0"
-        size  = 480
-        thin_provisioned = true
-    }
-
-    clone {
-        template_uuid = data.vsphere_virtual_machine.template.id
-    }
+  clone {
+    template_uuid = data.vsphere_virtual_machine.template.id
+  }
 }
